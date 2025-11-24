386003331b18ab11da4775c472e94a2d12e596af
mine_windows <windows@gmail.com>
2025-11-25 00:03:19 +0800

    提交代码2

--- showExcelSoap/src/test/java/com/showexcelsoap/ShowExcelSoapApplicationTests.java
+++ showExcelSoap/src/test/java/com/showexcelsoap/ShowExcelSoapApplicationTests.java
@@ -1,13 +1,13 @@
-package com.showexcelsoap;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ShowExcelSoapApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.showexcelsoap;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ShowExcelSoapApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
