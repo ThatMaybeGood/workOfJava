cc12cc6565daeac011238343d77638d29d47479c
mine_windows <windows@gmail.com>
2025-11-25 23:51:58 +0800

    提交请求合并调用的API

---
+++ message-data-service/src/main/java/com/example/messagedataservice/dto/ApiResponse.java
@@ -0,0 +1,24 @@
+package com.example.messagedataservice.dto;
+
+
+public class ApiResponse {
+    private ApiResponseHead head;
+    private ApiResponseBody body;
+
+
+    public ApiResponseHead getHead() {
+        return head;
+    }
+
+    public void setHead(ApiResponseHead head) {
+        this.head = head;
+    }
+
+    public ApiResponseBody getBody() {
+        return body;
+    }
+
+    public void setBody(ApiResponseBody body) {
+        this.body = body;
+    }
+}
